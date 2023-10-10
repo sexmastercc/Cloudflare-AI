6978230e30d9e693240f53a356c2077aecc985a5
LocalUser <121312523+localuser-isback@users.noreply.github.com>
2023-10-10 18:53:11 -0400

    Update worker.js
    alot of stuff

--- CF/worker.js
+++ CF/worker.js
@@ -12,16 +12,18 @@ function uuid() {
   }
   return uuid;
 }
+
 const chats = new Map();
+const maxMemory = 5
+const preprompt = "You are a helpful and responsive assistant, you answer questions directly and provide instruction unless told otherwise.";
 
 export default {
   async fetch(request, env) {
     const tasks = [];
-    const ai = new Ai(env.AI);
-
     const url = new URL(request.url);
     const query = decodeURIComponent(url.searchParams.get('q'));
     const id = url.pathname.substring(1);
+    const ai = new Ai(env.AI);
 
     if (!id) {
       const newId = uuid();
@@ -35,8 +37,11 @@ export default {
       chat = {
         messages: [],
         userId: id,
+        messageCount: 0,
       };
       chats.set(id, chat);
+      chat.messages.push({ role: 'system', content: preprompt });
+      tasks.push({ inputs: chat, response: chat.messages });
     }
 
     if (!query) {
@@ -52,23 +57,28 @@ export default {
       });
     }
 
-    if (query) {
-      if (query == "null") {
-        tasks.push({ inputs: chat, response: chat.messages });
-        return new Response(JSON.stringify(tasks), {
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '86400',
-          },
-        });
-      } else {
-        chat.messages.push({ role: 'user', content: query });
-        let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
-        chat.messages.push({ role: 'system', content: response });
+    if (query === "null") {
+      tasks.push({ inputs: chat, response: chat.messages });
+      return new Response(JSON.stringify(tasks), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Max-Age': '86400',
+        },
+      });
+    } else {
+      chat.messages.push({ role: 'user', content: query });
+      chat.messageCount += 1;
+
+      if (chat.messageCount >= maxMemory + 1) {
+        chat.messages = chat.messages.slice(-2);
+        chat.messageCount = 0;
       }
+
+      let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
+      chat.messages.push({ role: 'system', content: response });
     }
 
     tasks.push({ inputs: chat, response: chat.messages });
