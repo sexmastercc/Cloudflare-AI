cefdee48d4c600608d9147e8aa17c024904b79dd
LocalUser <121312523+localuser-isback@users.noreply.github.com>
2023-10-10 10:32:13 -0400

    Create worker.js

---
+++ CF/worker.js
@@ -0,0 +1,72 @@
+import { Ai } from './vendor/@cloudflare/ai';
+
+function uuid() {
+  let uuid = '';
+  const chars = 'abcdef0123456789';
+  for (let i = 0; i < 32; i++) {
+    const charIndex = Math.floor(Math.random() * chars.length);
+    uuid += chars[charIndex];
+    if (i === 7 || i === 11 || i === 15 || i === 19) {
+      uuid += '-';
+    }
+  }
+  return uuid;
+}
+
+const chats = new Map();
+
+export default {
+  async fetch(request, env) {
+    const tasks = [];
+    const ai = new Ai(env.AI);
+
+    const url = new URL(request.url);
+    const query = url.searchParams.get('q');
+    const id = url.pathname.substring(1);
+
+    if (!id) {
+      const newId = uuid();
+      const newUrl = `${url.origin}/${newId}`;
+      return Response.redirect(newUrl, 301);
+    }
+
+    let chat = chats.get(id);
+
+    if (!chat) {
+      chat = {
+        messages: [],
+        userId: id,
+      };
+      chats.set(id, chat);
+    } else if (!query) {
+      tasks.push({ inputs: chat, response: chat.messages });
+      return new Response(JSON.stringify(tasks), {
+        headers: { 
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Max-Age': '86400', 
+        },
+      });  
+    }
+
+    if (query) {
+      chat.messages.push({ role: 'user', content: query });
+      let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
+      chat.messages.push({ role: 'system', content: response });
+    }
+
+    tasks.push({ inputs: chat, response: chat.messages });
+
+    return new Response(JSON.stringify(tasks), {
+      headers: { 
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '86400', 
+      },
+    });    
+  },
+};
