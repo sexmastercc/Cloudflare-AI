fcc3676b7dd65c4b3829870cb6ac2ff7a23d8c4e
LocalUser <121312523+localuser-isback@users.noreply.github.com>
2023-10-22 14:34:31 -0400

    Update worker.js

--- CF/worker.js
+++ CF/worker.js
@@ -1,5 +1,5 @@
 //DO NOT TOUCH UNLESS YOU KNOW WHAT YOUR DOING!!
-import { Ai } from './vendor/@cloudflare/ai';
+import { Ai } from './vendor/@cloudflare/ai.js';
 
 //Current version of your API
 const version = "1.0.1";
